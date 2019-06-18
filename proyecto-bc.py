294dbc7bd5dbfed3f7503d252827bd856e058087
Cristian-Aguirre <aguirrecristian70@gmail.com>
2019-06-17 23:04:28 -0500

    actualizando

--- proyecto-bc.py
+++ proyecto-bc.py
@@ -9,8 +9,15 @@ from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna, generic_protein, generic_rna
 from Bio import SeqIO as seq
 from Bio import AlignIO as align
+#from Bio.Blast import NCBIWWW
+#from Bio.Blast import NCBIXML
 import os
 import subprocess
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+
+from Bio import Phylo
+import pylab
 
 
 class Prueba(QMainWindow):
@@ -37,14 +44,30 @@ class Prueba(QMainWindow):
         button3.setToolTip("<h4>Limpiar Texto</h4>")
         button3.clicked.connect(self.onClick3)
 
-        button4 = QPushButton("ARBOL FILOGENETICO!", self)
-        button4.setGeometry(50,550,150,30)
-        button4.setToolTip("<h4>Muestra el arbol creado</h4>")
+        button4 = QPushButton("BLAST!", self)
+        button4.move(100,550)
+        button4.setToolTip("<h4>Alinea con BLAST</h4>")
         button4.clicked.connect(self.onClick4)
+
+        button5 = QPushButton("ARBOL FILOGENETICO!", self)
+        button5.setGeometry(50,600,150,30)
+        button5.setToolTip("<h4>Muestra el arbol creado</h4>")
+        button5.clicked.connect(self.onClick5)
+
+        button6 = QPushButton("EPIPEDOBATES!", self)
+        button6.setGeometry(400,10,100,30)
+        button6.setToolTip("<h4>Escoger specie</h4>")
+        button6.clicked.connect(self.onClick6)
+
+        button7 = QPushButton("COLOSTETHUS!", self)
+        button7.setGeometry(700,10,100,30)
+        button7.setToolTip("<h4>Escoger familia</h4>")
+        button7.clicked.connect(self.onClick7)
 
         self.InitWindow()
 
     def InitWindow(self):
+        self.contador=0
 
         self.label2 = QLabel(self)
         self.label2.setPixmap(QPixmap('images/epipedobates_anthonyi.jpg'))
@@ -114,31 +137,38 @@ class Prueba(QMainWindow):
         self.alineamiento_multiple()
 
     def onClick2(self):
+       self.contador=0
        handle_output = open("Conjunto_fasta.fasta","w+")
        if self.checkbox1.isChecked():
-            f1 = open("secuencias/epipedobates-anthonyi-parcial.fasta","r")
-            texto1 = f1.read().strip()
+            self.f1 = open("secuencias/epipedobates-anthonyi-parcial.fasta","r")
+            texto1 = self.f1.read().strip()
             handle_output.write(texto1 +"\n\n")
+            self.contador+=1
        if self.checkbox2.isChecked():
-            f2 = open("secuencias/epipedobates-boulengei-parcial.fasta","r")
-            texto2 = f2.read().strip()
+            self.f2 = open("secuencias/epipedobates-boulengei-parcial.fasta","r")
+            texto2 = self.f2.read().strip()
             handle_output.write(texto2 +"\n\n")
+            self.contador+=1
        if self.checkbox3.isChecked():
-            f3 = open("secuencias/epipedobates-darwinwallacei-parcial.fasta","r")
-            texto3 = f3.read().strip()
+            self.f3 = open("secuencias/epipedobates-darwinwallacei-parcial.fasta","r")
+            texto3 = self.f3.read().strip()
             handle_output.write(texto3 +"\n\n")
+            self.contador+=1
        if self.checkbox4.isChecked():
-            f4 = open("secuencias/epipedobates-espinosai-parcial.fasta","r")
-            texto4 = f4.read().strip()
+            self.f4 = open("secuencias/epipedobates-espinosai-parcial.fasta","r")
+            texto4 = self.f4.read().strip()
             handle_output.write(texto4 +"\n\n")
+            self.contador+=1
        if self.checkbox5.isChecked():
-            f5 = open("secuencias/epipedobates-machalilla-parcial.fasta","r")
-            texto5 = f5.read().strip()
+            self.f5 = open("secuencias/epipedobates-machalilla-parcial.fasta","r")
+            texto5 = self.f5.read().strip()
             handle_output.write(texto5 +"\n\n")
+            self.contador+=1
        if self.checkbox6.isChecked():
-            f6 = open("secuencias/epipedobates-tricolor-parcial.fasta","r")
-            texto6 = f6.read().strip()
+            self.f6 = open("secuencias/epipedobates-tricolor-parcial.fasta","r")
+            texto6 = self.f6.read().strip()
             handle_output.write(texto6 +"\n\n")
+            self.contador+=1
        handle_output.close()
        ftotal = open("Conjunto_fasta.fasta","r")
        texto_total = ftotal.read().strip()
@@ -154,7 +184,92 @@ class Prueba(QMainWindow):
         self.cajadetexto.clear()
 
     def onClick4(self):
-        print("HOLA")
+        """print(self.contador)
+        if self.checkbox1.isChecked() and self.contador==1:
+            output_filename = "output.txt"
+            f = open(output_filename,'w')
+            sys.oudout = f
+            epipedobates_anthonyi = seq.read("secuencias/epipedobates-anthonyi-parcial.fasta", "fasta")
+            result_handle= NCBIWWW.qblast("blastn", "nt",epipedobates_anthonyi.seq)
+            blast_records = NCBIXML.parse(result_handle)
+            for b in blast_records:
+                for alignment in b.alignments:
+                    for hsp in alignment.hsps:
+                        print('\n****Alineamiento****', file=f)
+                        print('secuencia:', alignment.title, file=f)
+                        print('longitud:', alignment.length, file=f)
+                        print('e value:', hsp.expect, file=f)
+                        print(hsp.query[0:100] + '...', file=f)
+                        print(hsp.match[0:100] + '...', file=f)
+                        print(hsp.sbjct[0:100] + '...', file=f)
+            f1 = open("output.txt","r")
+            texto1 = f1.read().strip()
+            self.cajadetexto.setText(texto1)
+        if self.checkbox2.isChecked() and self.contador==1:
+            output_filename = "output.txt"
+            f = open(output_filename,'w')
+            sys.oudout = f
+            epipedobates_boulengei = seq.read("secuencias/epipedobates-boulengei-parcial.fasta", "fasta")
+            result_handle= NCBIWWW.qblast("blastn", "nt",epipedobates_boulengei.seq)
+            blast_records = NCBIXML.parse(result_handle)
+            for b in blast_records:
+                for alignment in b.alignments:
+                    for hsp in alignment.hsps:
+                        print('\n****Alineamiento****', file=f)
+                        print('secuencia:', alignment.title, file=f)
+                        print('longitud:', alignment.length, file=f)
+                        print('e value:', hsp.expect, file=f)
+                        print(hsp.query[0:100] + '...', file=f)
+                        print(hsp.match[0:100] + '...', file=f)
+                        print(hsp.sbjct[0:100] + '...', file=f)
+            f1 = open("output.txt","r")
+            texto1 = f1.read().strip()
+            self.cajadetexto.setText(texto1)
+"""
+    def onClick5(self):
+        with open("Conjunto_fasta.aln", "r") as aln:
+        #usar AlignIO tpara leer el archivo de alineamiento en formato 'clustal' format
+            alignment = align.read(aln, "clustal")
+        #calcular la  matriz de distancias
+        calculator = DistanceCalculator('identity')
+        # añade la matriz de  distancias al objeto calculator y lo retorna
+        dm = calculator.get_distance(alignment)
+
+        #Construir el arbol filogenetico aprtir de las distancias
+        constructor = DistanceTreeConstructor(calculator)
+
+        upgma_tree = constructor.upgma(dm)
+
+        Phylo.draw_ascii(upgma_tree)
+        Phylo.draw(upgma_tree)
+
+    def onClick6(self):
+        self.label2.show()
+        self.checkbox1.show()
+        self.label3.show()
+        self.checkbox2.show()
+        self.label4.show()
+        self.checkbox3.show()
+        self.label5.show()
+        self.checkbox4.show()
+        self.label6.show()
+        self.checkbox5.show()
+        self.label7.show()
+        self.checkbox6.show()
+
+    def onClick7(self):
+        self.label2.hide()
+        self.checkbox1.hide()
+        self.label3.hide()
+        self.checkbox2.hide()
+        self.label4.hide()
+        self.checkbox3.hide()
+        self.label5.hide()
+        self.checkbox4.hide()
+        self.label6.hide()
+        self.checkbox5.hide()
+        self.label7.hide()
+        self.checkbox6.hide()
 
 App = QApplication(sys.argv)
 window = Prueba()
